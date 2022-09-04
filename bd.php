370d4e3b12a8a45d13d48e87b7724cbae0c2505f
aaneleh <helenakurzgon@gmail.com>
2022-09-04 18:58:46 -0300

    home resumida

---
+++ bd.php
@@ -0,0 +1,17 @@
+<?php
+
+    function connect(){
+        $host = "localhost";
+        $bd_name = "blog";
+        $user = "root";
+        $pass = "";
+    
+        try {
+            $db = new PDO("mysql:host=$host;dbname=$bd_name", "$user", "$pass");
+            return $db;
+        } catch (PDOException $err){
+            echo 'Erro: '.$err->getMessage();
+        }
+    }
+
+?>
